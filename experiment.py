db09730a6e1a878586ef639241a19622ef06325d
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-05-17 15:22:09 -0400

    working with pytorch example mnist code

--- experiment.py
+++ experiment.py
@@ -1,33 +1,11 @@
+"""
 from model import Model
 import torch
+import torchvision
+import torchvision.datasets as datasets
 
-BATCH_SIZE = 64
 
-
-# Create random Tensors to hold inputs and outputs
-x = torch.randn(BATCH_SIZE, 784)
-y_labels = torch.randn(BATCH_SIZE, 10)
-model = Model(x, y_labels)
-
-learning_rate = 1e-4
-# parameters() returns an iterator over a list of the trainable model parameters in the same order in
-# which they appear in the network when traversed input -> output
-# (e.g.
-#       [weights b/w input and first hidden layer,
-#        bias b/w input and hidden layer 1,
-#        ... ,
-#        weights between last hidden layer and output,
-#        bias b/w hidden layer and output]
-# )
-optimizer = torch.optim.Adam(model.model.parameters(), lr=learning_rate)
-
-for i in range(500):
-    # The output- predictions of classification likelihoods- is produced by running
-    # the model on given input data.
-    y_pred = model.model(x)
-
-    loss = model.loss(y_pred, y_labels)
-
+def test():
     # Check if predictions are correct- a prediction is correct if the index of the highest value in the prediction
     # output is the same as the index of the highest value in the label for that sample.
     # For example (MNIST):
@@ -46,10 +24,178 @@ for i in range(500):
     # divided by the number of predictions it made (incorrect predictions evaluate to 0)
     accuracy = torch.mean(correct_prediction.type(torch.DoubleTensor))
 
-    print(i, loss.item(), accuracy)
+
+mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
+
+mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
+
+BATCH_SIZE = 64
+
+# Create random Tensors to hold inputs and outputs
+model = Model(mnist_trainset.train_data, mnist_trainset.train_labels)
+
+learning_rate = 1e-4
+# parameters() returns an iterator over a list of the trainable model parameters in the same order in
+# which they appear in the network when traversed input -> output
+# (e.g.
+#       [weights b/w input and first hidden layer,
+#        bias b/w input and hidden layer 1,
+#        ... ,
+#        weights between last hidden layer and output,
+#        bias b/w hidden layer and output]
+# )
+optimizer = torch.optim.Adam(model.model.parameters(), lr=learning_rate)
+
+for i in range(500):
+    # The output- predictions of classification likelihoods- is produced by running
+    # the model on given input data.
+    train_predictions = model.model(mnist_trainset.train_data)
+
+    loss = model.loss(train_predictions, mnist_trainset.train_labels)
+
+    print(i, loss.item())
 
     optimizer.zero_grad()
 
     loss.backward()
 
     optimizer.step()
+
+"""
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+def main():
+
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+
+    args = parser.parse_args()
+
+    # determines if CUDA should be used - only if available AND not disabled via arguments
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    # set a manual seed for random number generation
+    torch.manual_seed(args.seed)
+
+    # set the device on which to perform computations
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # arguments specific to CUDA computation
+    # num_workers: how many subprocesses to use for data loading - if set to 0, data will be loaded in the main process
+    # pin_memory: if True, the DataLoader will copy tensors into CUDA pinned memory before returning them
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+    # A PyTorch DataLoader combines a dataset and a sampler, and returns single- or multi-process iterators over
+    # the dataset.
+
+    # DataLoader for the training data.
+    # ARGUMENTS (in order):
+    # dataset (Dataset) – dataset from which to load the data.
+    # batch_size (int, optional) – how many samples per batch to load (default: 1).
+    # shuffle (bool, optional) – set to True to have the data reshuffled at every epoch (default: False).
+    train_loader = torch.utils.data.DataLoader(
+        # Use the MNIST dataset.
+        # ARGUMENTS (in order):
+        # root (string) – Root directory of dataset where processed/training.pt and processed/test.pt exist.
+        # train (bool, optional) – If True, creates dataset from training.pt, otherwise from test.pt.
+        # download (bool, optional) – If true, downloads the dataset from the internet and puts it in root directory.
+        #                                       If dataset is already downloaded, it is not downloaded again.
+        # transform (callable, optional) – A function/transform that takes in an PIL image and returns a transformed
+        #                                       version. E.g, transforms.RandomCrop
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
+
+
+if __name__ == '__main__':
+    main()
--- model.py
+++ model.py
@@ -1,12 +1,12 @@
 import torch
 
 class Model:
-    def __init__(self, x, y_labels):
+    def __init__(self, in_data, out_data):
 
         self.h1_dim = 100 # number of nodes in hidden layer 1
 
-        self.in_dim = int(list(x.size())[1]) # input dimension - 784 (28 * 28) for MNIST
-        self.out_dim = int(list(y_labels.size())[1]) # output dimension - 10 classes (digits 0-9) for MNIST
+        self.in_dim = int(list(in_data.size())[0]) # input dimension - 784 (28 * 28) for MNIST
+        self.out_dim = int(list(out_data.size())[0]) # output dimension - 10 classes (digits 0-9) for MNIST
 
         # Simple network with one hidden layer
         self.model = torch.nn.Sequential(
@@ -17,6 +17,6 @@ class Model:
 
         # Define the initial loss function as simple cross entropy, as the network will first be trained
         # on a single task (no Fisher Information incorporated into the loss function).
-        self.loss = torch.nn.MultiLabelSoftMarginLoss()
+        self.loss = torch.nn.CrossEntropyLoss()
 
 
