287aa0d2e9cf1de4e0e287c0fb1f97cb63e944d3
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-05-28 00:47:56 -0400

    working on simplifying code/ test/train loop

--- utils.py
+++ utils.py
@@ -27,7 +27,7 @@ def apply_permutation(image, permutation):
 
     return image
 
-def generate_new_mnist_task(train_dataset_size, validation_dataset_size, batch_size, kwargs, first_task):
+def generate_new_mnist_task(train_dataset_size, validation_dataset_size, batch_size, test_batch_size, kwargs, first_task):
 
     # TODO note the "spread" rather than travel method- as used in ariseff, also get rid of magic number
     # generate numpy array containing 0 - 783 in random order - a permutation "mask" to be applied to each image
@@ -46,13 +46,18 @@ def generate_new_mnist_task(train_dataset_size, validation_dataset_size, batch_s
         ]
 
     # TODO comment, also check if we need to alter the root for each new dataset
-    new_train_data, new_validation_data = \
+    train_data, validation_data = \
         D.dataset.random_split(datasets.MNIST('../data', train=True, transform=transforms.Compose(transformations)),
             [train_dataset_size, validation_dataset_size])
 
-    new_test_data = \
+    test_data = \
         datasets.MNIST('../data', train=False, transform=transforms.Compose(transformations))
 
-    train_loader = D.DataLoader(new_train_data, batch_size=batch_size, shuffle=True, **kwargs)
+    train_loader = D.DataLoader(train_data, batch_size=batch_size, shuffle=True, **kwargs)
 
+    validation_loader = D.DataLoader(validation_data, batch_size=1, shuffle=True, **kwargs)
+
+    test_loader = D.DataLoader(test_data, batch_size=test_batch_size, shuffle=True, **kwargs)
+
+    return train_loader, validation_loader, test_loader
 
