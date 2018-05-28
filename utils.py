37b5cbf8e9b1e66f668997bde82910dbdffae403
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-05-28 00:43:46 -0400

    working on simplifying code/ test/train loop

--- utils.py
+++ utils.py
@@ -27,26 +27,32 @@ def apply_permutation(image, permutation):
 
     return image
 
-def generate_new_mnist_task(train_dataset_size, validation_dataset_size, kwargs):
+def generate_new_mnist_task(train_dataset_size, validation_dataset_size, batch_size, kwargs, first_task):
 
     # TODO note the "spread" rather than travel method- as used in ariseff, also get rid of magic number
     # generate numpy array containing 0 - 783 in random order - a permutation "mask" to be applied to each image
     # in the MNIST dataset
     permutation = np.random.permutation(784)
 
-    # TODO comment, also check if we need to alter the root for each new dataset
-    new_train_data, new_validation_data = D.dataset.random_split(
-        datasets.MNIST('../data', train=True, transform=transforms.Compose([
+    transformations = \
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ] if first_task else \
+        [
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,)),
             transforms.Lambda(lambda x: apply_permutation(x, permutation))
-        ])), [train_dataset_size, validation_dataset_size])
+        ]
+
+    # TODO comment, also check if we need to alter the root for each new dataset
+    new_train_data, new_validation_data = \
+        D.dataset.random_split(datasets.MNIST('../data', train=True, transform=transforms.Compose(transformations)),
+            [train_dataset_size, validation_dataset_size])
 
     new_test_data = \
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,)),
-            transforms.Lambda(lambda x: apply_permutation(x, permutation))
-        ]))
+        datasets.MNIST('../data', train=False, transform=transforms.Compose(transformations))
 
-    train_loader = D.DataLoader(new_train_data, batch_size=args.batch_size, shuffle=True, **kwargs)
+    train_loader = D.DataLoader(new_train_data, batch_size=batch_size, shuffle=True, **kwargs)
+
+
