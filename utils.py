a7bfffb1778ad0a50b062e0faf2bd6b4c2988e77
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-06-05 18:38:10 -0400

    THIS COMMIT contains a few versions of the code (some less efficient than others) - REMEMBER not to use parameter.data during loss calculations, as this will ruin ewc accuracy (not tracked by autograd)

--- utils.py
+++ utils.py
@@ -162,8 +162,10 @@ def expand_model(model):
         model.lam
     )
 
-    # copy over old post-training weights
-    expanded_model.theta_stars = model.theta_stars
+    if model.ewc:
+        # copy over old post-training weights and Fisher info
+        expanded_model.theta_stars = model.theta_stars
+        expanded_model.list_of_FIMs = model.list_of_FIMs
 
     copy_weights_expanding(model, expanded_model)
 
@@ -557,7 +559,22 @@ def calculate_ewc_loss_prev_tasks(model):
     losses = []
 
     for parameter_index, parameter in enumerate(model.parameters()):
+        """
+        theta_star_template = (parameter.data.clone())
 
+        if len(list(model.theta_stars[parameter_index].size())) > 1:
+
+            for row in range(len(model.theta_stars[parameter_index])):
+                for col in range(len(model.theta_stars[parameter_index][row])):
+                    theta_star_template[row][col] = model.theta_stars[parameter_index][row][col]
+
+        else:
+
+            for value_index in range(len(model.theta_stars[parameter_index])):
+                theta_star_template[value_index] = model.theta_stars[parameter_index][value_index]
+        
+        theta_star = Variable(theta_star_template)
+        """
         theta_star = Variable(model.theta_stars[parameter_index])
         fisher = Variable(model.list_of_FIMs[parameter_index])
 
@@ -567,14 +584,22 @@ def calculate_ewc_loss_prev_tasks(model):
         # did not exist in the smaller network should NOT factor into the loss for this particular task and should
         # move freely. when theta stars are re-saved after training, movement of these weights will THEN be factored
         # into the loss function because we want to restrict their movements.
-        theta_star_size = list(theta_star.size())
 
-        parameter_as_theta_star_size = torch.index_select(parameter.data.clone(), 0, theta_star_size[0])
+        # todo fix this
+        theta_star_size = theta_star.size()
+
+        dim_0_indices = torch.arange(theta_star_size[0], dtype=torch.long)
+
+        parameter_as_theta_star_size = torch.index_select(parameter, 0, dim_0_indices)
 
         if len(theta_star_size) > 1:
-            parameter_as_theta_star_size = torch.index_select(parameter_as_theta_star_size, 1, theta_star_size[1])
+            dim_1_indices = torch.arange(theta_star_size[1], dtype=torch.long)
 
-        losses.append((fisher * (parameter_as_theta_star_size - theta_star) ** 2).sum())
+            parameter_as_theta_star_size = torch.index_select(parameter_as_theta_star_size, 1, dim_1_indices)
+
+
+        diff = (parameter - theta_star)
+        losses.append((fisher * (diff ** 2)).sum())
 
     return (model.lam / 2.0) * sum(losses)
 
