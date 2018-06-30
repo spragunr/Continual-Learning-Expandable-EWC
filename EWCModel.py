0746de880a187796bfac1ed7ab8f6dd9cdfa4b85
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-06-30 18:59:19 -0400

    working on cuda compatability

--- EWCModel.py
+++ EWCModel.py
@@ -67,14 +67,14 @@ class EWCModel(ExpandableModel):
         # parameter index
         for fisher_diagonal_index in range(len(self.sum_Fx)):
 
-            self.sum_Fx[fisher_diagonal_index].cuda().add_(self.list_of_fisher_diags[fisher_diagonal_index])
+            self.sum_Fx[fisher_diagonal_index].add_(self.list_of_fisher_diags[fisher_diagonal_index])
 
         # add the fisher diagonal for each parameter multiplied (element-wise) by that parameter's current weight values
         # to the existing sum_Fx_Wx entry at the corresponding parameter index
         for fisher_diagonal_index in range(len(self.sum_Fx_Wx)):
 
             self.sum_Fx_Wx[fisher_diagonal_index] = torch.addcmul(
-                self.sum_Fx_Wx[fisher_diagonal_index].cuda(),
+                self.sum_Fx_Wx[fisher_diagonal_index],
                 self.list_of_fisher_diags[fisher_diagonal_index],
                 current_weights[fisher_diagonal_index])
 
