049f0c57d478f58e997e7350bc807665653f3479
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-07-02 19:51:22 -0400

    working on variable learning rate

--- EWCModel.py
+++ EWCModel.py
@@ -261,8 +261,8 @@ class EWCModel(ExpandableModel):
                 # parameters
                 loss.backward()
 
-                # if task_number > 1: # todo change to hasattr() call
-                #     self.tune_variable_learning_rates()
+                if task_number > 1: # todo change to hasattr() call
+                     self.tune_variable_learning_rates()
 
                 # Simplified abstraction provided by PyTorch which uses a single statement to update all model parameters
                 # according to gradients (with respect to the last loss function on which .backward() was called and
