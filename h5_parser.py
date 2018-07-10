e8d77cd7a1cf35ce132eecb92e43c632c3d05a6e
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-07-10 07:13:36 -0400

    working on saving metrics for final figure evaluation

--- h5_parser.py
+++ h5_parser.py
@@ -1,12 +1,13 @@
 import h5py
 import numpy as np
+from copy import deepcopy
 
 f = h5py.File('test_results.hdf5', 'r')
 
 datasets = []
 
 for key in f.keys():
-    datasets.append(f[key])
+    datasets.append(deepcopy(f[key]))
 
 f.close()
 
