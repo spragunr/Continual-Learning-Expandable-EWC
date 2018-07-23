8d177b78d78213a4f771e747452ddece22c3ec4f
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-07-23 02:15:33 -0400

    fixing command line args

--- setup.py
+++ setup.py
@@ -13,14 +13,89 @@ from pathlib import Path
 
 
 def parse_arguments():
+
     parser = argparse.ArgumentParser(description='Variable Capacity Network for Continual Learning')
 
-    experiment = input("\nWHICH TYPE OF EXPERIMENT WOULD YOU LIKE TO RUN?[cifar/mnist/custom]:  ").strip().lower()
+    parser.add_argument('--experiment', type=str, default='custom', metavar='EXPERIMENT',
+                        help='preconfigured experiment to run: mnist or cifar (defaults to custom)')
 
-    if experiment == 'mnist':
+    parser.add_argument('--batch-size', type=int, default=4, metavar='BS',
+                        help='input batch size for training')
+
+    parser.add_argument('--test-batch-size', type=int, default=4, metavar='TBS',
+                        help='input batch size for testing')
+
+    parser.add_argument('--epochs', type=int, default=2, metavar='E',
+                        help='number of epochs to train')
+
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate')
+
+    parser.add_argument('--l2-reg-penalty', type=float, default=0.0, metavar='L2',
+                        help='l2 regularization penalty (weight decay) (default: 0.0)')
+
+    parser.add_argument('--lam', type=float, default=150, metavar='LAM',
+                        help='ewc lambda value (fisher multiplier) (default: 15)')
+
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='SGD momentum (default: 0.0)')
+
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='seed for RNGs')
+
+    parser.add_argument('--log-interval', type=int, default=2000, metavar='N',
+                        help='how many batches to wait before logging training status (default 10)')
+
+    # [train dataset size] = [full MNIST train set (60,000)] - [validation set size]
+    parser.add_argument('--train-dataset-size', type=int, default=50000, metavar='TDS',
+                        help='number of images in the training dataset')
+
+    parser.add_argument('--validation-dataset-size', type=int, default=0, metavar='VDS',
+                        help='number of images in the validation dataset')
+
+    # size of hidden layer in MLP in neurons OR initial number of filters in conv network
+    parser.add_argument('--hidden-size', type=int, default=0, metavar='HS',
+                        help='# neurons in each hidden layer of MLP OR # filters in conv resnet')
+
+    # 28 x 28 pixels = 784 pixels per MNIST image, 32 x 32 = 1024 for CIFAR 10
+    parser.add_argument('--input-size', type=int, default=1024, metavar='IS',
+                        help='size of each input data sampe to the network (default 784 (28 * 28))')
+
+    # 10 classes - digits 0-9 for MNIST, 100 for CIFAR 100
+    parser.add_argument('--output-size', type=int, default=10, metavar='OS',
+                        help='size of the output of the network (default 10)')
+
+    # e.g. 2 to double the size of the network when expansion occurs
+    parser.add_argument('--scale-factor', type=int, default=2, metavar='ES',
+                        help='the factor by which to scale the size of network layers upon expansion')
+
+    # accuracy threshold (minimum) required on most recent task- if not met, network will reset to pre-training state
+    # and expand - set to 0 to disable automatic network expansion
+    parser.add_argument('--accuracy-threshold', type=int, default=0, metavar='AT',
+                        help='accuracy threshold (minimum) required on all tasks')
+
+    # dataset on which to train/test model
+    parser.add_argument('--dataset', type=str, default='cifar', metavar='DS',
+                        help='dataset on which to train/test model (cifar100 or mnist)')
+
+    # number of tasks
+    parser.add_argument('--tasks', type=int, default=3, metavar='T',
+                        help='number of tasks')
+
+    parser.add_argument('--output-file', type=str, default='custom.h5', metavar='OUTPUT FILE',
+                        help='h5 file for storage of experimental results')
+
+    parser.add_argument('--nets', nargs='+', type=str, default=['VanillaCNN'], metavar='NETS',
+                        help='neural net classes to train')
+
+    args = parser.parse_args()
+
+    if args.experiment == 'mnist':
 
         print('|-----[CONDUCTING PERMUTED MNIST EXPERIMENT]-----|')
-        args = parser.parse_args()
 
         args.batch_size = 100
         args.test_batch_size = 1000
@@ -52,10 +127,10 @@ def parse_arguments():
             else:
                 print("{:_<30}{:_>30}".format(k, ", ".join(args_dict.get(k))))
 
-    elif experiment == 'cifar':
+    elif args.experiment == 'cifar':
 
         print('|-----[CONDUCTING INCREMENTAL CIFAR 100 EXPERIMENT]-----|')
-        args = parser.parse_args()
+
         args.batch_size = 4
         args.test_batch_size = 4
         args.epochs = 2
@@ -85,84 +160,10 @@ def parse_arguments():
                 print("{:_<30}{:_>30}".format(k, args_dict.get(k)))
             else:
                 print("{:_<30}{:_>30}".format(k, ", ".join(args_dict.get(k))))
-    else:
+
+    elif args.experiment == 'custom':
+
         print('|-----[CUSTOM EXPERIMENT- DEFAULT HYPERPARAMETERS USED WHERE NOT SPECIFIED]-----|')
-
-        parser.add_argument('--batch-size', type=int, default=4, metavar='BS',
-                            help='input batch size for training')
-
-        parser.add_argument('--test-batch-size', type=int, default=4, metavar='TBS',
-                            help='input batch size for testing')
-
-        parser.add_argument('--epochs', type=int, default=2, metavar='E',
-                            help='number of epochs to train')
-
-        parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
-                            help='learning rate')
-
-        parser.add_argument('--l2-reg-penalty', type=float, default=0.0, metavar='L2',
-                            help='l2 regularization penalty (weight decay) (default: 0.0)')
-
-        parser.add_argument('--lam', type=float, default=150, metavar='LAM',
-                            help='ewc lambda value (fisher multiplier) (default: 15)')
-
-        parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
-                            help='SGD momentum (default: 0.0)')
-
-        parser.add_argument('--no-cuda', action='store_true', default=False,
-                            help='disables CUDA training')
-
-        parser.add_argument('--seed', type=int, default=1, metavar='S',
-                            help='seed for RNGs')
-
-        parser.add_argument('--log-interval', type=int, default=2000, metavar='N',
-                            help='how many batches to wait before logging training status (default 10)')
-
-        # [train dataset size] = [full MNIST train set (60,000)] - [validation set size]
-        parser.add_argument('--train-dataset-size', type=int, default=50000, metavar='TDS',
-                            help='number of images in the training dataset')
-
-        parser.add_argument('--validation-dataset-size', type=int, default=0, metavar='VDS',
-                            help='number of images in the validation dataset')
-
-        # size of hidden layer in MLP in neurons OR initial number of filters in conv network
-        parser.add_argument('--hidden-size', type=int, default=0, metavar='HS',
-                            help='# neurons in each hidden layer of MLP OR # filters in conv resnet')
-
-        # 28 x 28 pixels = 784 pixels per MNIST image, 32 x 32 = 1024 for CIFAR 10
-        parser.add_argument('--input-size', type=int, default=1024, metavar='IS',
-                            help='size of each input data sampe to the network (default 784 (28 * 28))')
-
-        # 10 classes - digits 0-9 for MNIST, 100 for CIFAR 100
-        parser.add_argument('--output-size', type=int, default=10, metavar='OS',
-                            help='size of the output of the network (default 10)')
-
-        # e.g. 2 to double the size of the network when expansion occurs
-        parser.add_argument('--scale-factor', type=int, default=2, metavar='ES',
-                            help='the factor by which to scale the size of network layers upon expansion')
-
-        # accuracy threshold (minimum) required on most recent task- if not met, network will reset to pre-training state
-        # and expand - set to 0 to disable automatic network expansion
-        parser.add_argument('--accuracy-threshold', type=int, default=0, metavar='AT',
-                            help='accuracy threshold (minimum) required on all tasks')
-
-        # dataset on which to train/test model
-        parser.add_argument('--dataset', type=str, default='cifar', metavar='DS',
-                            help='dataset on which to train/test model (cifar100 or mnist)')
-
-        # number of tasks
-        parser.add_argument('--tasks', type=int, default=3, metavar='T',
-                            help='number of tasks')
-
-        parser.add_argument('--output-file', type=str, default='custom.h5', metavar='OUTPUT FILE',
-                            help='h5 file for storage of experimental results')
-
-        parser.add_argument('--nets', nargs='+', type=str, default=['VanillaCNN'], metavar='NETS',
-                            help='neural net classes to train')
-
-        args = parser.parse_args()
-
-        args.experiment = 'custom'
 
         args_dict = vars(args)
 
@@ -171,6 +172,10 @@ def parse_arguments():
                 print("{:_<30}{:_>30}".format(k, args_dict.get(k)))
             else:
                 print("{:_<30}{:_>30}".format(k, ", ".join(args_dict.get(k))))
+
+    else:
+
+        raise ValueError("Invalid experiment type selected: {}!\n".format(args.experiment))
 
     return args
 
