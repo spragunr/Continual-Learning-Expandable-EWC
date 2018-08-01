8582a72b5a06fe32394119be1251c7f06f1df2cb
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-08-01 05:16:19 -0400

    trying with simple CNN

--- CNN.py
+++ CNN.py
@@ -18,29 +18,29 @@ class CNN(ExpandableModel):
 
     def forward(self, x):
 
-        # x = self.pool(F.relu(self.conv1(x)))
-        # x = self.pool(F.relu(self.conv2(x)))
-        # x = x.view(-1, 16 * 5 * 5)
-        # x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
-        # self.y = x
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        self.y = x
 
 
-        self.y = self.resnet.forward(x)
+        # self.y = self.resnet.forward(x)
 
         return self.y
 
     def build(self):
 
-        # self.conv1 = nn.Conv2d(3, 6, 5)
-        # self.pool = nn.MaxPool2d(2, 2)
-        # self.conv2 = nn.Conv2d(6, 16, 5)
-        # self.fc1 = nn.Linear(16 * 5 * 5, 240)
-        # self.fc2 = nn.Linear(240, 120)
-        # self.fc3 = nn.Linear(120, 100)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 240)
+        self.fc2 = nn.Linear(240, 120)
+        self.fc3 = nn.Linear(120, 100)
 
-        self.resnet = ResNet18(self.output_size, self.hidden_size)
+        # self.resnet = ResNet18(self.output_size, self.hidden_size)
 
     def test(self, test_loaders, threshold, args):
 
