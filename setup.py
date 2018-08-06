5fb065cd8745443b07a508bfba549084d8c7f7c3
Andrew Jones <andrew@localhost.localdomain>
2018-08-06 17:31:39 -0400

    modified alexnet bsed on github findings for CIFAR

--- setup.py
+++ setup.py
@@ -139,8 +139,8 @@ def parse_arguments():
 
         print('|-----[CONDUCTING INCREMENTAL CIFAR 100 EXPERIMENT]-----|')
 
-        args.batch_size = 128
-        args.test_batch_size = 100
+        args.batch_size = 4
+        args.test_batch_size = 4
         args.epochs = 10
         args.lr = 0.1
         args.l2_reg_penalty = 0.0
