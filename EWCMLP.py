0b40a8423207f42b62e0215d777fb891bc501095
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-08-25 18:53:26 -0400

    re-enabled variable learning rate

--- EWCMLP.py
+++ EWCMLP.py
@@ -318,8 +318,8 @@ class EWCMLP(MLP):
                 # parameters
                 loss.backward()
 
-                #if task_number > 1: # todo change to hasattr() call
-                    # self.tune_variable_learning_rates() # TODO: RE-ENABLE(DISABLED TO PRODUCE FAILURE)
+                if task_number > 1: # todo change to hasattr() call
+                    self.tune_variable_learning_rates() 
 
                 # Simplified abstraction provided by PyTorch which uses a single statement to update all model parameters
                 # according to gradients (with respect to the last loss function on which .backward() was called and
