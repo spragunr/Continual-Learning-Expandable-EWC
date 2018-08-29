451d757c189d5720262218fbcbf7de6967486caf
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-08-29 03:48:41 -0400

    looking into alexnet randomness

--- EWCCNN.py
+++ EWCCNN.py
@@ -220,6 +220,8 @@ class EWCCNN(CNN):
                 # .to(device):
                 #   set the device (CPU or GPU) to be used with data and target to device variable (defined in main())
                 data, target = Variable(data).to(self.device), Variable(target).to(self.device)
+                
+                print(target)
 
                 # Gradients are automatically accumulated- therefore, they need to be zeroed out before the next backward
                 # pass through the network so that they are replaced by newly computed gradients at later training iterations,
