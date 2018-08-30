0e096fe794a39e01f895c805f760a2ffecc83ccc
Andrew Jones <jonesaj@dukes.jmu.edu>
2018-08-30 01:46:34 -0400

    failure experiments prepped

--- EWCMLP.py
+++ EWCMLP.py
@@ -327,9 +327,10 @@ class EWCMLP(MLP):
                 # Backward pass: compute gradient of the loss with respect to model
                 # parameters
                 loss.backward()
-
-                if task_number > 1: # todo change to hasattr() call
-                    self.tune_variable_learning_rates() 
+                
+                # TODO re-enable, disabled to induce network failure
+                # if task_number > 1: # todo change to hasattr() call
+                    # self.tune_variable_learning_rates() 
 
                 # Simplified abstraction provided by PyTorch which uses a single statement to update all model parameters
                 # according to gradients (with respect to the last loss function on which .backward() was called and
